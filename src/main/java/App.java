122f859aed4bdd9f64a8ada3e50ed8c0b19d5ed0
lpelczar <lukepelczar@gmail.com>
2018-06-12 20:42:16 +0200

    Add main method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,21 @@
+import loader.CsvLoader;
+import loader.SudokuFileLoader;
+
 public class App {
 
     public static void main(String[] args) {
 
+        if (args.length == 1) {
+            String filename = args[0];
+            SudokuFileLoader sudokuFileLoader = new CsvLoader();
+            byte[] sudokuArray = sudokuFileLoader.load(filename);
+            if (sudokuArray != null) {
+                // Solve sudoku
+            } else {
+                System.out.println("Invalid sudoku file!");
+            }
+        } else {
+            System.out.println("You need to specify sudoku filename as console argument!");
+        }
     }
 }

5cb4ee95cdf92581ed212b5d3d972d71935dcf1b
martastaporek <staporekmarta1@gmail.com>
2018-06-13 13:31:08 +0200

    Implement test wrong csv file size

--- src/test/java/loader/CsvLoaderTest.java
+++ src/test/java/loader/CsvLoaderTest.java
@@ -1,7 +1,30 @@
 package loader;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.Random;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CsvLoaderTest {
+
+    CsvLoader loader;
+
+    @BeforeEach
+    void setup(){
+        this.loader = new CsvLoader();
+    }
+
+
+    @Test
+    void testPassWrongSizeFile(){
+
+        assertNull(this.loader.load("src/test/test_resources/only_digits.csv"));
+    }
+    
+
+
 
 }

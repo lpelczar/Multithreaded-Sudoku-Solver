e88db93bc2de33f17b205e486623c0c1e99eabf6
martastaporek <staporekmarta1@gmail.com>
2018-06-13 20:57:36 +0200

    Implement check repeated values in sudoku

--- src/main/java/utils/PreliminarySudokuChecker.java
+++ src/main/java/utils/PreliminarySudokuChecker.java
@@ -4,18 +4,20 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class PreliminarySudokuChecker {
-
-    //private static final List <Integer> NONRECURING_VALUES = new ArrayList<>(Arrays.asList(1, 2 ,3, 4, 5, 6, 7, 8, 9));
-
+    
     public static boolean isSudoku(int [] cellValues){
 
         List<Integer> listCellValue = Arrays.stream(cellValues).boxed().collect(Collectors.toList());
+        if(!checkBoxes(listCellValue)){
+            return false;
+        }
         if(!checkRowsHorizontally(listCellValue)){
             return false;
         }
         if(!checkRowsVertically(listCellValue)){
             return false;
         }
+
         return true;
     }
 
@@ -23,10 +25,8 @@ public class PreliminarySudokuChecker {
 
         for(int i = 0; i < cellValues.size(); i = i + 9){
             List <Integer> row = cellValues.subList(i, i+9);
-            for(int j = 1; j <= 9; j++ ) {
-                if (Collections.frequency(row, j) > 1) {
-                    return false;
-                }
+            if(!checkDuplicates(row)){
+                return false;
             }
 
         }
@@ -42,12 +42,39 @@ public class PreliminarySudokuChecker {
                    row.add(cellValues.get(i));
                }
            }
-           for(int j = 1; j <= 9; j++ ) {
-               if (Collections.frequency(row, j) > 1) {
-                   return false;
-               }
+           if(!checkDuplicates(row)){
+               return false;
            }
         }
         return true;
+    }
+
+    private static boolean checkBoxes(List <Integer> cellValues){
+        for(int i = 0; i < cellValues.size(); i += 3) {
+            List<Integer> box = new ArrayList<>();
+            for(int j = 0; j <27; j+=9) {
+                box.add(cellValues.get(i + j));
+                box.add(cellValues.get(i + j + 1));
+                box.add(cellValues.get(i + j + 2));
+            }
+            if(!checkDuplicates(box)){
+                return false;
+            }
+
+
+        }
+
+        return true;
+    }
+
+    private static boolean checkDuplicates(List <Integer> collection){
+
+        for (int j = 1; j < 9; j++) {
+            if (Collections.frequency(collection, j) > 1) {
+                return false;
+            }
+        }
+        return true;
+
     }
 }

d996a8016687c2b0f00e7bf7eaa6901240d90701
martastaporek <staporekmarta1@gmail.com>
2018-06-13 22:09:48 +0200

    Refactor class

--- src/main/java/utils/PreliminarySudokuChecker.java
+++ src/main/java/utils/PreliminarySudokuChecker.java
@@ -4,7 +4,7 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class PreliminarySudokuChecker {
-    
+
     public static boolean isSudoku(int [] cellValues){
 
         List<Integer> listCellValue = Arrays.stream(cellValues).boxed().collect(Collectors.toList());
@@ -14,11 +14,8 @@ public class PreliminarySudokuChecker {
         if(!checkRowsHorizontally(listCellValue)){
             return false;
         }
-        if(!checkRowsVertically(listCellValue)){
-            return false;
-        }
 
-        return true;
+        return (checkRowsVertically(listCellValue));
     }
 
     private static boolean checkRowsHorizontally(List<Integer> cellValues){
@@ -35,13 +32,12 @@ public class PreliminarySudokuChecker {
     }
 
     private static boolean checkRowsVertically(List <Integer> cellValues){
-        for(int i = 0; i < cellValues.size(); i = i + 9 ){
+
+        for(int i = 0; i < 9; i ++ ){
             List<Integer> row = new ArrayList<>();
-           for(int j = 0; j < 9; j++){
-               if(i%9 == j){
-                   row.add(cellValues.get(i));
+           for(int j = i; j < cellValues.size(); j+=9){
+                   row.add(cellValues.get(j));
                }
-           }
            if(!checkDuplicates(row)){
                return false;
            }
@@ -50,12 +46,14 @@ public class PreliminarySudokuChecker {
     }
 
     private static boolean checkBoxes(List <Integer> cellValues){
-        for(int i = 0; i < cellValues.size(); i += 3) {
+
+
+        for(int i = 0; i < cellValues.size(); i += 27) {
             List<Integer> box = new ArrayList<>();
-            for(int j = 0; j <27; j+=9) {
-                box.add(cellValues.get(i + j));
-                box.add(cellValues.get(i + j + 1));
-                box.add(cellValues.get(i + j + 2));
+            for(int j = i; j < i + 27; j+=9) {
+                box.add(cellValues.get(j));
+                box.add(cellValues.get(j + 1));
+                box.add(cellValues.get(j + 2));
             }
             if(!checkDuplicates(box)){
                 return false;
@@ -64,10 +62,38 @@ public class PreliminarySudokuChecker {
 
         }
 
+        for(int i = 3; i < cellValues.size(); i += 27) {
+            List<Integer> box = new ArrayList<>();
+            for(int j = i; j < i + 27; j+=9) {
+                box.add(cellValues.get(j));
+                box.add(cellValues.get(j + 1));
+                box.add(cellValues.get(j + 2));
+            }
+            if(!checkDuplicates(box)){
+                return false;
+            }
+
+
+        }
+
+        for(int i = 6; i < cellValues.size(); i += 27) {
+            List<Integer> box = new ArrayList<>();
+            for(int j = i; j < i + 27; j+=9) {
+                box.add(cellValues.get(j));
+                box.add(cellValues.get(j + 1));
+                box.add(cellValues.get(j + 2));
+            }
+            if(!checkDuplicates(box)){
+                return false;
+            }
+
+        }
+
         return true;
     }
 
     private static boolean checkDuplicates(List <Integer> collection){
+
 
         for (int j = 1; j < 9; j++) {
             if (Collections.frequency(collection, j) > 1) {

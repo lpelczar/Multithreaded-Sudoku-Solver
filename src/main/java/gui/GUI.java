626967e99c6b31d4ef281ef928942ed612b451a7
Michal Gruszka <m.gruszka215@gmail.com>
2018-06-20 17:01:40 +0200

    Add cells and solve button

--- src/main/java/gui/GUI.java
+++ src/main/java/gui/GUI.java
@@ -1,8 +1,12 @@
 package gui;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,7 +19,23 @@ public class GUI extends Application {
 
         primaryStage.setTitle("Sudoku Solver");
 
+        GridPane gpLayout = new GridPane();
+        gpLayout.setPadding(new Insets(20, 20, 20, 20));
+        gpLayout.setHgap(8);
+        gpLayout.setVgap(8);
+
+        for (int i = 0; i < cells.length; i++) {
+            TextField tf = new TextField();
+            cells[i] = tf;
+            gpLayout.getChildren().add(tf);
+            GridPane.setConstraints(tf, i % 9, i / 9);
+        }
+
+        Button solveButton = new Button("Solve!");
+
         VBox v = new VBox(0);
+        v.getChildren().addAll(gpLayout, solveButton);
+        v.setAlignment(Pos.CENTER);
         primaryStage.setScene(new Scene(v, 350, 450));
         primaryStage.show();
     }
